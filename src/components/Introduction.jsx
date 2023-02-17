f2d325a839eab7b4588ee37e3370f03ef538d1a1
santhosh s <subramanisanthosh00@gmail.com>
2023-02-17 15:57:04 +0530

    Done

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,13 @@
+const Introduction = ()=>{
+    return(
+        <>
+            <h1>Santhosh S</h1>
+            <p>FrontEnd Web Developer, Educator at Geekster</p>
+            <a href="" target={"_blank"} style={{marginRight:"10px"}}>Linkedin</a>
+            <a href="" target={"_blank"}>Github</a>
+            <hr />
+        </>
+    )
+}
+
+export default Introduction;
