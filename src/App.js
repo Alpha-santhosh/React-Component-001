4a91155114b3c250b0bf61005dcda94aff950455
santhosh s <subramanisanthosh00@gmail.com>
2023-02-17 16:05:14 +0530

    Adding Experience component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Experience from './components/Experience';
 import Introduction from './components/Introduction';
 
 function App() {
   return (
     <>
       <Introduction/>
+      <Experience/>
     </>
   );
 }
