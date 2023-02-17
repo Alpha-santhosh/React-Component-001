4af5a5b44d45c92a2e416de63b3200eac840c168
santhosh s <subramanisanthosh00@gmail.com>
2023-02-17 16:12:31 +0530

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Experience from './components/Experience';
+import Hobbies from './components/Hobbies';
 import Introduction from './components/Introduction';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <>
       <Introduction/>
       <Experience/>
+      <Skills/>
+      <Hobbies/>
     </>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,17 @@
+const Skills = ()=>{
+    return(
+        <>
+            <h2>Skills</h2>
+            <ul>
+                <li>Java</li>
+                <li>JavaScipt</li>
+                <li>Testing</li>
+                <li>API Designing</li>
+                <li>System design</li>
+            </ul>
+            <hr />
+        </>
+    )
+}
+
+export default Skills;
